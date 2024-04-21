e31d41d7d99c8ac346db2f5cc0d97581ed60c7f5
Michael Samorokov <michael.samorokov@gmail.com>
2024-04-21 17:22:14 -0600

    updated MAUI

--- ViewViewLoadFailRepro/MainPageViewModel.cs
+++ ViewViewLoadFailRepro/MainPageViewModel.cs
@@ -27,7 +27,7 @@ namespace ViewViewLoadFailRepro
 
         private string BuildUrl()
         {
-            return "https://gpx.studio/?state=%7b%22urls%22%3a%5b%22https%3a%2f%2fat1.samorokov.online%2fapi%2fGpxFile%3ftrackId%3d3f709779-0be0-4863-bdd7-08d1674f6ccd%22%5d%7d&distance&direction&slope&imperial";
+            return "https://gpx.studio/?state=%7b%22urls%22%3a%5b%22https%3a%2f%2fat1.samorokov.online%2fapi%2fGpxFile%3ftrackId%3d3f709779-0be0-4863-bdd7-08d1674f6ccd%22%5d%7d&distance&direction&slope&imperial&embed";
         }
     }
 }
